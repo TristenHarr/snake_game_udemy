e5b924519d50747f4e27b6d8d610d1b11d2b5e7c
Tristen Harr <tristenharr@gmail.com>
2024-01-10 00:10:17 -0600

    initial commit

--- www/index.ts
+++ www/index.ts
@@ -1,71 +1,139 @@
-import init, {World} from "snake_game";
+import init, { World, Direction, GameStatus } from "snake_game";
+import { rnd } from "./utils/rnd";
 
-init().then(_ => {
-    const CELL_SIZE = 30;
+init().then((wasm) => {
+  const CELL_SIZE = 20;
+  const WORLD_WIDTH = 3;
+  const snakeSpawnIdx = rnd(WORLD_WIDTH * WORLD_WIDTH);
 
-    const world = World.new();
-    const worldWidth = world.width();
+  const world = World.new(WORLD_WIDTH, snakeSpawnIdx, Direction.Right);
+  const worldWidth = world.width();
 
-    const canvas = document.getElementById("snake-canvas");
-    const ctx = canvas.getContext("2d");
+  const gameStatus = document.getElementById("game-status");
+  const gameControlBtn = document.getElementById("game-control-btn");
+  const pointsStatus = document.getElementById("points");
+  const canvas = <HTMLCanvasElement>document.getElementById("snake-canvas");
+  const ctx = canvas.getContext("2d");
 
-    canvas.height = worldWidth*CELL_SIZE;
-    canvas.width = worldWidth*CELL_SIZE;
+  canvas.height = worldWidth * CELL_SIZE;
+  canvas.width = worldWidth * CELL_SIZE;
 
-    function drawWorld(){
-        ctx.beginPath();
-        for (let x = 0; x < worldWidth + 1; x++){
-            ctx.moveTo(CELL_SIZE*x, 0);
-            ctx.lineTo(CELL_SIZE*x, worldWidth*CELL_SIZE);
+  gameControlBtn.addEventListener("click", (_) => {
+    const status = world.game_status();
+    if (status === undefined) {
+      world.start_game();
+      play();
+    } else {
+      location.reload();
+    }
+  });
+
+  document.addEventListener("keydown", (e) => {
+    switch (e.code) {
+      case "ArrowUp":
+        world.change_snake_dir(Direction.Up);
+        break;
+      case "ArrowRight":
+        world.change_snake_dir(Direction.Right);
+        break;
+      case "ArrowDown":
+        world.change_snake_dir(Direction.Down);
+        break;
+      case "ArrowLeft":
+        world.change_snake_dir(Direction.Left);
+        break;
+      case "Space":
+        const status = world.game_status();
+        if (status === undefined) {
+          world.start_game();
+          play();
+        } else {
+          location.reload();
         }
+        break;
+      default:
+        console.log(e);
+        break;
+    }
+  });
 
-        for (let y = 0; y<worldWidth+1; y++){
-            ctx.moveTo(0, CELL_SIZE * y);
-            ctx.lineTo(worldWidth*CELL_SIZE, y*CELL_SIZE);
-        }
-        ctx.stroke();
+  function drawWorld() {
+    ctx.beginPath();
+
+    for (let x = 0; x < worldWidth + 1; x++) {
+      ctx.moveTo(CELL_SIZE * x, 0);
+      ctx.lineTo(CELL_SIZE * x, worldWidth * CELL_SIZE);
     }
 
-    function drawSnake() {
-        const snakeIdx = world.snake_head_idx();
-        const col = snakeIdx % worldWidth;
-        const row = Math.floor(snakeIdx / worldWidth);
+    for (let y = 0; y < worldWidth + 1; y++) {
+      ctx.moveTo(0, CELL_SIZE * y);
+      ctx.lineTo(worldWidth * CELL_SIZE, CELL_SIZE * y);
+    }
+
+    ctx.stroke();
+  }
+
+  function drawReward() {
+    const idx = world.reward_cell();
+    const col = idx % worldWidth;
+    const row = Math.floor(idx / worldWidth);
+
+    ctx.beginPath();
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    ctx.stroke();
+  }
+
+  function drawSnake() {
+    const snakeCells = new Uint32Array(
+      wasm.memory.buffer,
+      world.snake_cells(),
+      world.snake_length()
+    );
+
+    snakeCells
+      .slice()
+      .reverse()
+      .forEach((cellIdx, i) => {
+        const col = cellIdx % worldWidth;
+        const row = Math.floor(cellIdx / worldWidth);
+
+        ctx.fillStyle = i === snakeCells.length - 1 ? "#7878db" : "#000000";
+
         ctx.beginPath();
-        ctx.fillRect(
-            col * CELL_SIZE,
-            row * CELL_SIZE,
-            CELL_SIZE,
-            CELL_SIZE
-        );
-        ctx.stroke();
+        ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+      });
+
+    ctx.stroke();
+  }
+
+  function drawGameStatus() {
+    gameStatus.textContent = world.game_status_text();
+    pointsStatus.textContent = world.points().toString();
+  }
+
+  function paint() {
+    drawWorld();
+    drawSnake();
+    drawReward();
+    drawGameStatus();
+  }
+
+  function play() {
+    const status = world.game_status();
+    if (status === GameStatus.Won || status === GameStatus.Lost){
+        gameControlBtn.textContent = "Replay";
+        return;
     }
+    const fps = 2;
+    setTimeout(() => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      world.step();
+      paint();
+      // the method takes a callback to invoked before the next repaint
+      requestAnimationFrame(play);
+    }, 1000 / fps);
+  }
 
-    function paint() {
-        drawWorld();
-        drawSnake();
-    }
-
-    let ctr = 0;
-    let timeoutId;
-    function update() {
-        // if (ctr > 100){
-        //     console.log("DONE");
-        //     return null;
-        // }
-
-        setTimeout(() => {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            drawWorld();
-            drawSnake();
-            world.update();
-            // The method request animation frame takes a callback to be invoked before the next repaint.
-            requestAnimationFrame(update);
-            console.log(world.snake_head_idx());
-            ctr += 1;
-            console.log("Frame ", ctr);
-        }, 100);
-    }
-
-    paint();
-    update();
-})
+  paint();
+});
---
+++ www/notes.md
@@ -0,0 +1,6 @@
+We need to compile Rust to webassembly code.
+
+WASM can be executed in the browser and runs at native speed.
+
+3d apps, cad, virtual reality, system applications, games, etc.
+
